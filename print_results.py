7a37c4efd95223041f2bb132e5686b66da02050e
Joanna Carrión Pérez <77378686+Joanna20Carrion@users.noreply.github.com>
2024-11-17 17:41:31 -0500

    print_results.py

--- print_results.py
+++ print_results.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/print_results.py
 #                                                                             
-# PROGRAMMER: Klein T Kugara
-# DATE CREATED: 11 July 2023
+# PROGRAMMER: Joanna Carrion
+# DATE CREATED:
 # REVISED DATE: 
 # PURPOSE: Create a function print_results that prints the results statistics
 #          from the results statistics dictionary (results_stats_dic). It 
