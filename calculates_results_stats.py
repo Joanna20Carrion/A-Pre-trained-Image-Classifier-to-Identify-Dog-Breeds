4e1bc28982c9a90289b35048a6110ab1ea7432b4
Joanna Carrión Pérez <77378686+Joanna20Carrion@users.noreply.github.com>
2024-11-17 17:25:06 -0500

    calculates_results_stats.py

--- calculates_results_stats.py
+++ calculates_results_stats.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# */AIPND-revision/intropyproject-classify-pet-images/calculates_results_stats.py
+# calculates_results_stats.py
 #                                                                             
-# PROGRAMMER:Klein T Kugara
-# DATE CREATED: 10 July 2023                                 
-# REVISED DATE: 
+# PROGRAMMER:Joanna Carrion
+#
 # PURPOSE: Create a function calculates_results_stats that calculates the 
 #          statistics of the results of the programrun using the classifier's model 
 #          architecture to classify the images. This function will use the 
