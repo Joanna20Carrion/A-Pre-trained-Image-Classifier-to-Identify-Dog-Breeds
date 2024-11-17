865272ff8acbeff8907f0ff98d34c2a27d98b8a1
Joanna Carrión Pérez <77378686+Joanna20Carrion@users.noreply.github.com>
2024-11-17 17:20:34 -0500

    adjust_results4_isadog.py

--- adjust_results4_isadog.py
+++ adjust_results4_isadog.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/adjust_results4_isadog.py
 #                                                                             
-# PROGRAMMER: Joanna Carrion                              
-# REVISED DATE: 
+# PROGRAMMER: Joanna Carrion  
+#
 # PURPOSE: Create a function adjust_results4_isadog that adjusts the results 
 #          dictionary to indicate whether or not the pet image label is of-a-dog, 
 #          and to indicate whether or not the classifier image label is of-a-dog.
